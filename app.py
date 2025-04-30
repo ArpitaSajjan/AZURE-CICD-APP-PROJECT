a6c420e633832a4fbffd3b0b3b10196b7e81dba0
Zakariya Abdullahi <your_email@example.com>
2025-04-30 14:18:18 +0100

    Updated app.py with latest changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from opencensus.trace.samplers import ProbabilitySampler
 
 app = Flask(__name__)
 
-# Set up Application Insights exporter
+
 middleware = FlaskMiddleware(
     app,
     exporter=AzureExporter(connection_string='InstrumentationKey=99ff427e-6f0a-429c-b87c-6760b062e5cb;IngestionEndpoint=https://ukwest-0.in.applicationinsights.azure.com/;LiveEndpoint=https://ukwest.livediagnostics.monitor.azure.com/;ApplicationId=11d7eeb3-bd20-4ce8-8c0e-7c77af344ffe'),
